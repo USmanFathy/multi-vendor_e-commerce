03aa19458aa80a5091d42823586e0cf135ee49ba
usman ahmed fathy <91374170+USmanFathy@users.noreply.github.com>
2023-10-14 17:55:04 +0300

    authenticaion by fortify

--- config/app.php
+++ config/app.php
@@ -162,6 +162,8 @@ return [
          * Package Service Providers...
          */
 
+        App\Providers\FortifyServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
